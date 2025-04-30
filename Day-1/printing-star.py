d7bfad61772e606a234e6718fb925b2782d635f1
edddddr <endt6342@gmail.com>
2025-04-30 10:30:40 -0700

    len word

--- Day-1/printing-star.py
+++ Day-1/printing-star.py
@@ -1,2 +1,8 @@
-print("Day 1 - Python print Function\nThe" \
-" function is declared like this\nprint('what to print)")
+# print("Day 1 - Python print Function\nThe" 
+# " function is declared like this\nprint('what to print')")
+
+# Input
+# print("Hello " + input("what is your name?"))
+
+# Exercise on input
+print(len(input("Enter any word and I will give u the length ")))
