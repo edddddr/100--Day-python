90bf2b8f959c47e601a202cf319ad8018c18faab
edddddr <endt6342@gmail.com>
2025-04-30 11:23:51 -0700

    Separetely

--- Day-1/printing-star.py
+++ Day-1/printing-star.py
@@ -7,6 +7,12 @@
 # Exercise on input
 # print(len(input("Enter any word and I will give u the length ")))
 
-name = input("Enter name : ")
-print(name)
+# Variables
+# name = input("Enter name : ")
+# print("Hello " + name )
+
+# separately using variables
+name = input("What is your name? ")
+length = len(name)
+print(length)
 
