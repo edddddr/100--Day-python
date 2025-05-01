0df669f7f0d2e0ef12f1b35739fc8739d0162278
edddddr <endt6342@gmail.com>
2025-04-30 19:01:15 -0700

    Project_1

---
+++ Day-1/bandName.py
@@ -0,0 +1,4 @@
+print("Welcome to the Vand Name Generator.")
+city = input("What's name of the city your grew up in? \n")
+cat_name = input("What's your pet's name? \n")
+print("Your band naem could be " + city +" "+ cat_name)
