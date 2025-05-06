118bb39a8ae03639665f047ec4fc304ed6320f3a
edddddr <endt6342@gmail.com>
2025-05-06 14:00:02 -0700

    On data type

---
+++ Day-2/dataTypes.py
@@ -0,0 +1,25 @@
+# Data Types
+
+#String
+##concatination
+print("Hello"[4])
+
+#Integer +,-
+
+123_456_678
+
+#Float
+3.4380
+
+#Boolean
+True
+False
+
+
+#Type Error, Type Checking and Type Conversion
+num = input("Enter a number")
+str(num)
+new_num = num[0]
+new_num1 = num[1]
+
+print(int(new_num) + int(new_num1))
