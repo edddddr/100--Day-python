85eab3e5d19dab076983c04c128a9e517337f9aa
edddddr <endt6342@gmail.com>
2025-05-07 02:30:28 -0700

    Revised

--- Day-1/printing-star.py
+++ Day-1/printing-star.py
@@ -1,18 +1,19 @@
-# print("Day 1 - Python print Function\nThe" 
-# " function is declared like this\nprint('what to print')")
+print("Day 1 - Python print Function\nThe" 
+" function is declared like this\nprint('what to print')")
 
 # Input
 print("Hello " + input("what is your name?"))
 
 # Exercise on input
-# print(len(input("Enter any word and I will give u the length ")))
+print(len(input("Enter any word and I will give u the length ")))
 
 # Variables
-# name = input("Enter name : ")
-# print("Hello " + name )
+name = input("Enter name : ")
+print("Hello " + name )
 
 # separately using variables
-# name = input("What is your name? ")
-# length = len(name)
-# print(length)
+name = input("What is your name? ")
+length = len(name)
+print(length)
 
+# line 7 and 14 are similar output but the way the code writen is diff | It's how to program
--- Day-2/dataTypes.py
+++ Day-2/dataTypes.py
@@ -25,7 +25,25 @@ num = input("Enter a number")
 
 new_num = int(num[0])
 new_num1 = int(num[1])
-
+ 
 result = new_num + new_num1
 
 print(result)
+
+# PEMDASLR | Priority 
+# P = parentheses
+# E = Exponent
+# M = Multiplication
+# D = Division
+# A = Addition
+# S = Subtraction
+# LR = Left & Right
+
+ 
+# Number Manipulation and F String in python
+# round()'
+round(2.3333, 2) # the output will be 2.33
+8 // 3 # the division symbols make it integer even the result is float.
+#f-String -   it ables us to add a numbers of value on the string by doing f" {}"
+
+
