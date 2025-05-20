d08511639d992e96a1286591907f6067cc46ea93
edddddr <endt6342@gmail.com>
2025-05-19 19:19:19 -0900

    oddeven

---
+++ Day-3/oddev.py
@@ -0,0 +1,10 @@
+number = int(input("Input a number to test the number odd or even"))
+
+if number % 2 == 0:
+    print(f"The number {number} is enven")
+else:
+    print(f"The number {number} is odd")
+    
+    
+
+    
