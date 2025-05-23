c9302b5c78d7b3347bdd0a1321a67d1138d11bc9
edddddr <endt6342@gmail.com>
2025-05-22 20:44:14 -0900

    AVG is don wiht two loops

---
+++ Day-4/averageHeight.py
@@ -0,0 +1,18 @@
+
+student_height = input("A list student height : ").split()
+
+
+for n in range(0, len(student_height)):
+    student_height[n] = int(student_height[n])
+
+
+count = 0
+sum= 0
+for n in student_height:
+  sum +=n 
+  count +=1
+
+avg = sum / count
+print(f"Average legnth is: {round(avg)}") 
+
+
