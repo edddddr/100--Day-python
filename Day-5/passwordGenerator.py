233b8923c9c34c68150c21592575b6220e530208
edddddr <endt6342@gmail.com>
2025-05-22 23:30:19 -0900

    level one is completed

--- Day-5/passwordGenerator.py
+++ Day-5/passwordGenerator.py
@@ -5,22 +5,31 @@
 
 import random
 
-n_l = 3
-n_S = 2
-n_N = 2
+
+# Password generator
+
+# Random number
+
+# Level one ++++++
+print("Welcome to the PyPassword Generatoro! 🔑\n")
+nr_letters = int(input("How many letter would do you like in your password? \n"))
+nr_symbols = int(input("How many symbols would do you like? \n"))
+nr_number =  int(input("How many numbers would do you like? \n"))
 
 password = ''
-nr_letters = ['a','b','c','d','e','f','g', 'h', 'i', 'j']
-nr_symbols = ['!','@','#','$','%','^','&','*','(', ')']
-nr_number = [1,2,4,5,6,7,8,9,0]
+letters = ['a','b','c','d','e','f','g', 'h', 'i', 'j']
+symbols = ['!','@','#','$','%','^','&','*','(', ')']
+number = [1,2,4,5,6,7,8,9,0]
 
-for i in range(0, n_l):
-    password += nr_letters[random.randint(0,9)]
+
+for i in range(0, nr_letters):
+    password += letters[random.randint(0,9)]
     
-for i in range(0, n_S):
-    password += nr_symbols[random.randint(0,9)]
+for i in range(0, nr_symbols):
+    password += symbols[random.randint(0,9)]
 
-for i in range(0, n_N):
-    password += str(nr_number[random.randint(0,8)])
+for i in range(0, nr_number):
+    password += str(number[random.randint(1,9)])
+
 
 print(password)
