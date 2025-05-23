05756900523ebc7c0cdd830c3b9ee693c1228da9
edddddr <endt6342@gmail.com>
2025-05-23 01:48:24 -0900

    level - Hard

--- Day-5/passwordGenerator.py
+++ Day-5/passwordGenerator.py
@@ -14,22 +14,32 @@ import random
 print("Welcome to the PyPassword Generatoro! 🔑\n")
 nr_letters = int(input("How many letter would do you like in your password? \n"))
 nr_symbols = int(input("How many symbols would do you like? \n"))
-nr_number =  int(input("How many numbers would do you like? \n"))
+nr_number =  int(input("How many numbers would do you like? \n")) 
 
+passwordList = ''
 password = ''
 letters = ['a','b','c','d','e','f','g', 'h', 'i', 'j']
 symbols = ['!','@','#','$','%','^','&','*','(', ')']
 number = [1,2,4,5,6,7,8,9,0]
+passwordList 
 
 
 for i in range(0, nr_letters):
-    password += letters[random.randint(0,9)]
+    passwordList += letters[random.randint(0,9)] + ' '
     
 for i in range(0, nr_symbols):
-    password += symbols[random.randint(0,9)]
+    passwordList += symbols[random.randint(0,9)] + ' '
 
 for i in range(0, nr_number):
-    password += str(number[random.randint(1,9)])
+    passwordList += str(number[random.randint(0,8)]) + ' '
+
+
+passwordList = passwordList.split()
+ 
+random.shuffle(passwordList)
+    
+for char in passwordList:
+    password += char
 
 
 print(password)
