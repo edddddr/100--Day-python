931c0e48fd7b0589894c6cbc49eeb073a1e023b1
edddddr <endt6342@gmail.com>
2025-05-27 23:10:21 -0900

    Hagman stage 4 displaying with characters

--- Day-7/day-7-Hangman-1-start.py
+++ Day-7/day-7-Hangman-1-start.py
@@ -2,10 +2,13 @@ import random
 
 
 words = ["ardvark", "baboon", "camel" ]
+display = []
 chosen_word = random.choice(words)
 end_of_game = False
+stage = ["Hangman", "Hangm", "Hang", "Ha", "H"]
+lives = 4
 
-display = []
+
 
 
 
@@ -21,13 +24,22 @@ while not end_of_game:
         letter = chosen_word[position]
         if guess == letter:
             display[position] = letter
+    
+    if not guess in display:
+        print(stage[lives])
+        print(lives)
+        if lives == 0:
+            print("You lose!")
+            end_of_game = True
+        lives = lives -1
+
 
     print(display)
 
     if not "_" in display:
         end_of_game = True
         print("You win! ")
-
+    
 
 
 
