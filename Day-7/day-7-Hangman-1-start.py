06193d83eb389705abb5d71f78ceff0c1504342a
edddddr <endt6342@gmail.com>
2025-05-27 23:51:11 -0900

    Stage 7 the last level which is importing a list and notfing a user if alrady exists and checking the letter is in the chosen word 'Erro'

--- Day-7/day-7-Hangman-1-start.py
+++ Day-7/day-7-Hangman-1-start.py
@@ -1,15 +1,14 @@
 import random
+import words
 
-
-words = ["ardvark", "baboon", "camel" ]
 display = []
-chosen_word = random.choice(words)
+chosen_word = random.choice(words.word_list)
 end_of_game = False
 stage = ["Hangman", "Hangm", "Hang", "Ha", "H"]
 lives = 4
 
 
-
+guessed_words = []
 
 
 for n in range(len(chosen_word)):
@@ -20,6 +19,11 @@ print(f"The chosen word is : {chosen_word}")
 while not end_of_game:
     guess = input("Guess a word : ").lower()
 
+    if guess in guessed_words:
+        print(f'"{guess}" word is alrady guessed befor, guess again!')
+    if guess not in chosen_word:
+        print(f' "{guess}" letter is not in the word  ')
+
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
         if guess == letter:
@@ -28,6 +32,7 @@ while not end_of_game:
     if not guess in display:
         print(stage[lives])
         print(lives)
+        guessed_words += guess
         if lives == 0:
             print("You lose!")
             end_of_game = True
