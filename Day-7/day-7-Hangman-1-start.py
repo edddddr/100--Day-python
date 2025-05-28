d2760324111c4f34658a6676cbbe016d7c01f119
edddddr <endt6342@gmail.com>
2025-05-27 22:07:15 -0900

    stage 1&2

--- Day-7/day-7-Hangman-1-start.py
+++ Day-7/day-7-Hangman-1-start.py
@@ -2,14 +2,33 @@ import random
 
 
 words = ["ardvark", "baboon", "camel" ]
-guess = input("enter a letter : ").lower()
-
 chosen_word = random.choice(words)
+end_of_game = False
+
+display = []
+
+
+
+for n in range(len(chosen_word)):
+    display+="_"
+
 print(f"The chosen word is : {chosen_word}")
 
-for letter in chosen_word:
-    if letter == guess:
-        print("True")
-    else:
-        print("false")
+while not end_of_game:
+    guess = input("Guess a word : ").lower()
+
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if guess == letter:
+            display[position] = letter
+
+    print(display)
+
+    if not "_" in display:
+        end_of_game = True
+        print("You win! ")
+
+
+
+
 
