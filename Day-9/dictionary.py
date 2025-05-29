fd36e362a1b7c5a4b0b1b87122622741a8bc8973
edddddr <endt6342@gmail.com>
2025-05-29 06:50:44 -0700

    Dictionaries

---
+++ Day-9/dictionary.py
@@ -0,0 +1,6 @@
+programming_dictionary = {
+    "Bug": "An error llsd lkjfdis kjiosfl kjiwef lkjoiwejf oijf oisduj",
+    "Function": "A piece of code that youe can easily call over and over again."
+}
+
+print(programming_dictionary["Bug"])
