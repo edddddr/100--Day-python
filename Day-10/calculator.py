03eeac02df9d616ef90008dd22f5a45c2514d430
edddddr <endt6342@gmail.com>
2025-05-29 23:25:13 -0900

    problem always Data type

--- Day-10/calculator.py
+++ Day-10/calculator.py
@@ -19,24 +19,25 @@ operations = {
     '*': mul,
     '/': div
     }
+def calculator():
+    num1 = float(input("What's the first number? : "))
+    for symbol in operations:
+        print(symbol)
 
-num1 = int(input("What's the first number? : "))
-for symbol in operations:
-    print(symbol)
+    # to continue
+    should_continue = True
+    while should_continue:
+        operation_symbol = input("Peek operation from the line: ")
+        num2 = float(input("What's the next number? : "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num2} {operation_symbol} {num2} : {answer}")
 
-# to continue
-should_continue = True
-while should_continue:
-   operation_symbol = input("Peek operation from the line: ")
-   num2 = int(input("What's the next number? : "))
-   calculation_function = operations[operation_symbol]
-   answer = calculation_function(num1, num2)
-   print(f"{num2} {operation_symbol} {num2} : {answer}")
+        if  input(f"Type 'y' to continue with {answer}, or 'n' to start a new calculation : ") == 'y':
+                num1 = answer
+        else:
+            calculator()
+        
 
-   if  input(f"Type 'y' to continue with {answer}, or 'n' to exit : ") == 'n':
-        should_continue = False
-        os.system('cls')       
-        exit()
-    
-
+calculator()
 
