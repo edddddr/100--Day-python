395033b321c2211c51d1208431cfbd3ef73f39fe
edddddr <endt6342@gmail.com>
2025-05-29 23:07:28 -0900

    Instructor's code full respect :

--- Day-10/calculator.py
+++ Day-10/calculator.py
@@ -21,29 +21,22 @@ operations = {
     }
 
 num1 = int(input("What's the first number? : "))
-num2 = int(input("What's the second number? : "))
 for symbol in operations:
     print(symbol)
-
-
-operation_symbol = input("Peek operation from the line: ")
-
-first_calculation_function = operations[operation_symbol]
-answer = first_calculation_function(num1, num2)
-print(f"{num2} {operation_symbol} {num2} : {answer}")
 
 # to continue
 should_continue = True
 while should_continue:
-    result = input(f"Type 'y' to continue calculatign with {answer} or type 'n' to exit:")
-    if result == 'n':
-        os.system('cls')
+   operation_symbol = input("Peek operation from the line: ")
+   num2 = int(input("What's the next number? : "))
+   calculation_function = operations[operation_symbol]
+   answer = calculation_function(num1, num2)
+   print(f"{num2} {operation_symbol} {num2} : {answer}")
+
+   if  input(f"Type 'y' to continue with {answer}, or 'n' to exit : ") == 'n':
+        should_continue = False
+        os.system('cls')       
         exit()
-    else:
-        operation_symbol = input("Pick an operator : ")
-        num3 = int(input("What's the next number? : "))
-        second_calculation_function = operations[operation_symbol]
-        answer = second_calculation_function(first_calculation_function(num1, num2,), num3)
-        # print(f"{num2} {operation_symbol} {num2} : {answer}")
+    
 
 
