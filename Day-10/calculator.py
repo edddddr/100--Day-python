60b2ce3db38dd8cce5835468ec3bc54ae8df6f70
edddddr <endt6342@gmail.com>
2025-05-29 22:59:05 -0900

    student's code

--- Day-10/calculator.py
+++ Day-10/calculator.py
@@ -1,4 +1,4 @@
-
+import os
 # Calculator
 
 def add(n1, n2):
@@ -28,9 +28,22 @@ for symbol in operations:
 
 operation_symbol = input("Peek operation from the line: ")
 
-calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
-
+first_calculation_function = operations[operation_symbol]
+answer = first_calculation_function(num1, num2)
 print(f"{num2} {operation_symbol} {num2} : {answer}")
+
+# to continue
+should_continue = True
+while should_continue:
+    result = input(f"Type 'y' to continue calculatign with {answer} or type 'n' to exit:")
+    if result == 'n':
+        os.system('cls')
+        exit()
+    else:
+        operation_symbol = input("Pick an operator : ")
+        num3 = int(input("What's the next number? : "))
+        second_calculation_function = operations[operation_symbol]
+        answer = second_calculation_function(first_calculation_function(num1, num2,), num3)
+        # print(f"{num2} {operation_symbol} {num2} : {answer}")
 
 
