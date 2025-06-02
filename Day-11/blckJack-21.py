f69ffd9be4b0c09d740a5b1504073b5d8eda7ab9
edddddr <endt6342@gmail.com>
2025-06-02 00:43:07 -0900

    BlackJack is 21

--- Day-11/blckJack-21.py
+++ Day-11/blckJack-21.py
@@ -1,39 +1,75 @@
 import random
+import os
 
 cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 
+user_chosen = []
+computer_chosen = []
+
+def generator():
+        ch_card = []
+        random1 = random.randint(0,12)
+        ch_card.append(cards[random1])
+        random2 = random.randint(0,12)
+        ch_card.append(cards[random2])
+        random3 = random.randint(0,12)
+        ch_card.append(cards[random3])
+        
+        return ch_card
+
+
+
+def blackjack():
+    should_continue = True
+    
+    while should_continue:
+        answer1 = input("Do you want to play BlackJack 'y' or 'n' : ")
+        user_chosen = generator()
+        computer_chosen = generator()
+        if answer1 == 'y':
+            print(f"    Your cards: [{user_chosen[0]},{ user_chosen[1]}]")
+            print(f"    Computer's first card: {computer_chosen[0]}")
+            answer2 = input("Type 'y' to get another card, type 'n' to pass : ")
+            if answer2 == 'n':
+                print(f"    Your final hand : [{user_chosen[0]}, {user_chosen[1]}], final score : {sum_of(user_chosen[0],user_chosen[1])}")
+                print(f"    Computer's hand final : [{computer_chosen[0]}, {computer_chosen[1]}], final score {sum_of(computer_chosen[0], computer_chosen[1])}")
+
+                final_result_checker(sum_of(user_chosen[0],user_chosen[1]), sum_of(computer_chosen[0], computer_chosen[1]))
+              
+            else:
+                sum_of_user = sum_of(user_chosen[0],user_chosen[1], user_chosen[2])
+                sum_of_computer = sum_of(computer_chosen[0], computer_chosen[1])
+
+                print(f"    Your cards: [{user_chosen[0]}, {user_chosen[1]}, {user_chosen[2]}], current  score: {sum_of_user}")
+                print(f"    Computer's first card: [{computer_chosen[0]}], final score {sum_of_computer}")
+                print(f"    Your final hand : [{user_chosen[0]}, {user_chosen[1]}, {user_chosen[2]}], final score : {sum_of_user}")
+                print(f"    Computer's hand final : [{computer_chosen[0]}, {computer_chosen[1]}], final score {sum_of_computer}")
+                final_result_checker(sum_of_user, sum_of_computer)
+        else:
+            should_continue = False
+            os.system('cls')
+            exit()
+
+
+
+# Functions
 def sum_of(num1=0, num2=0, num3=0):
     sum = num1 + num2 + num3
     return sum
 
-for n in range(1,3):
-    random1 = random.randint(0,12)
-    user_chosen += cards[random1]
-    random2 = random.randint(0,12)
-    computer_chosen += cards[random2]
+def final_result_checker(sum_of_user, sum_of_computer):
 
-# def random_generator():
-#     return cards(random.randint(0,12))
-
-want_to_play = True
-response2 = 'y'
-while want_to_play:
-    response1 = input("Do you want to play a game Blackjack? 'y' or 'n' : ")
-    while response2 == 'y':
-        if response1 == 'n':
-            print(f"You final had : {user_chosen[0,1]} your current score : {sum_of(0,1,2)}")
-            print(f"Computer final had : {computer_chosen}")
-            
-            exit()
-        else:
-
-            print(f"Your card is : {user_chosen} your current score : {sum_of(user_chosen[0],user_chosen[1],user_chosen[2])}")
-            print(f"Computer first card: {computer_chosen[0]}")
-            response2 = input("Type 'y' to get another card, type 'n' to pass : ")
-            if response2 == 'n':
-                print(f'your final had: {user_chosen}, final score is : {sum_of(user_chosen[0],user_chosen[1],user_chosen[2])}')
-
-
+    if sum_of_user > 21:
+        print("You wentover. You lose 😭")
+    elif sum_of_user > sum_of_computer:
+         print("You win")
+    elif sum_of_computer > sum_of_user:
+         print("You lose")
+    else:
+        print("Draw . . .")
+         
+# Function call
+blackjack()
 
 
 
