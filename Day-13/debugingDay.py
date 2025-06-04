2f7f5e542787fc710913009a1287877e113578f5
edddddr <endt6342@gmail.com>
2025-06-04 09:30:36 -0900

    Buged

--- Day-13/debugingDay.py
+++ Day-13/debugingDay.py
@@ -24,6 +24,25 @@
 #     print("You are Gen Z. ")
 
 # Fix the Error
-age = int(input("How old are you? "))
-if age > 18:
-    print(f"You can drive at age {age}")
+# age = int(input("How old are you? "))
+# if age > 18:
+#     print(f"You can drive at age {age}")
+
+# Fix the Bug with print method
+# pages = 0
+# word_per_page = 0
+# page = int(input("Number of page : "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words = page * word_per_page
+# print(total_words)
+
+# Use a debuger
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+    b_list.append(new_item)
+    print(b_list)
+
+mutate([1,2,3,4,8,13])
+        
