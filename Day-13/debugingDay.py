f81c18aae1b82b68cc5fc1c8bf247eae4bec5c3d
edddddr <endt6342@gmail.com>
2025-06-03 22:37:22 -0900

    play with computer

--- Day-13/debugingDay.py
+++ Day-13/debugingDay.py
@@ -9,8 +9,16 @@
 
 
 # Reproduce the Bug
-from random import randint
-dice_imgs = ['1️', ' 2️⃣', '3️⃣', '4️⃣','5️⃣', '6️⃣']
-dice_num = randint(1,5)
-print(dice_num)
-print(dice_imgs[dice_num])
+# from random import randint
+# dice_imgs = ['1️', ' 2️⃣', '3️⃣', '4️⃣','5️⃣', '6️⃣']
+# dice_num = randint(1,5)
+# print(dice_num)
+# print(dice_imgs[dice_num])
+
+
+# Play with computer
+year = int(input("What is year of birth? "))
+if year > 1980 and year < 1994:
+    print("you are mellenial.")
+elif year >= 1994:
+    print("You are Gen Z. ")
