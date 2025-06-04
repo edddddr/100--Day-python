8b52c65de2d8a43c224c9c9a565d71e74fa3eb8e
edddddr <endt6342@gmail.com>
2025-06-03 22:27:10 -0900

    Reproduce the Bug

--- Day-13/debugingDay.py
+++ Day-13/debugingDay.py
@@ -1,9 +1,16 @@
 
 # Describe problem
-def my_function():
-    for n in range(1,21):
-        if n == 20:
-            print("You got it")
+# def my_function():
+#     for n in range(1,21):
+#         if n == 20:
+#             print("You got it")
 
-my_function()
+# my_function()
 
+
+# Reproduce the Bug
+from random import randint
+dice_imgs = ['1️', ' 2️⃣', '3️⃣', '4️⃣','5️⃣', '6️⃣']
+dice_num = randint(1,5)
+print(dice_num)
+print(dice_imgs[dice_num])
