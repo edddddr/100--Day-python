53fd31df1b5121b6dcea97feb4d7e340b0ca5ea6
edddddr <endt6342@gmail.com>
2025-06-11 01:06:01 -0900

    The class is classic

--- Day-17/quiz_game/main.py
+++ Day-17/quiz_game/main.py
@@ -1,6 +1,21 @@
-class Question:
-    def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
-        
-    
+from question_model import Question  
+from data import question_data
+from quiz_brian import QuizBrain
+
+question_bank = []
+
+for question in question_data:
+    question_text = question['text']
+    question_answer = question['answer']
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+     quiz.next_question()
+
+print("You've completed the quiz")
+print(f"your final score was: {quiz.score}/ {quiz.question_number}")
+
