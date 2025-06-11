127b2d0ea25d2f8b41d3871e6c29f46d1f17e360
edddddr <endt6342@gmail.com>
2025-06-10 22:26:19 -0900

    recoe the coffee machine from procecdural programming to object oriented programming

--- Day-16/main.py
+++ Day-16/main.py
@@ -3,3 +3,29 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 
+coffe_menu = Menu()
+coffee_machine = MoneyMachine()
+
+
+is_on = True
+
+while is_on:
+        
+    option = coffe_menu.get_items()
+    answer = input(f"What do you want? {option} : ")
+    if option == 'off':
+           is_on = False
+    if option == 'report':
+        coffee_maker.report() 
+        coffee_machine.report() 
+    else:
+        type = coffe_menu.find_drink(answer)
+
+        coffee_maker = CoffeeMaker()
+        if (coffee_maker.is_resource_sufficient(type)):
+                proccessed_money  = coffee_machine.process_coins()
+                if coffee_machine.make_payment(proccessed_money):
+                        coffee_maker.make_coffee(type)
+
+
+
