ecbb72bc0c940a6dcaafec6211d8a14163dbacc8
edddddr <endt6342@gmail.com>
2025-06-12 05:37:48 -0900

    It's all about how to utilize a package and tha ability to read a Documents . . .

---
+++ Day-18/main.py
@@ -0,0 +1,17 @@
+import random
+from turtle import Turtle as t, Screen
+
+turtle = t()
+
+
+
+# for _ in range(1,360):
+#     turtle.circle(160, 360)  # draw a semicircle
+
+turtle.circle(120, 360)
+turtle.position()
+
+
+
+
+
