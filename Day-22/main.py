7737851bb0d9739c1c11e4f0af6e20614ff3a4aa
edddddr <endt6342@gmail.com>
2025-06-18 01:45:11 -0900

    delete the paddle line import paddle method and crate left and right paddle let see

--- Day-22/main.py
+++ Day-22/main.py
@@ -1,29 +1,19 @@
 from turtle import Turtle, Screen 
-
+from paddle import Paddle
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("pong ⚾⚾⚾")
 screen.tracer(0)
 
-
-paddle1 = Turtle('square')
-paddle1.color('white')
-paddle1.shapesize(stretch_wid=5, stretch_len=1)
-paddle1.penup()
-paddle1.setpos(350, 0)
-
-def move_up():
-    y_position = paddle1.ycor() + 20
-    paddle1.goto(paddle1.xcor(), y_position)
-
-def move_down():
-    y_position = paddle1.ycor() - 20
-    paddle1.goto(paddle1.xcor(), y_position)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((350, 0))
 
 
-screen.onkey(move_up, 'Up')
-screen.onkey(move_down, 'Down')
+screen.onkey(r_paddle.move_up, 'Up')
+screen.onkey(r_paddle.move_down, 'Down')
+screen.onkey(l_paddle.move_up, 'Up')
+screen.onkey(l_paddle.move_down, 'Down')
 screen.listen()
 
 game_is_on = True
