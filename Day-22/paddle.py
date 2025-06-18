2bc1ef027ef39e764ba333649915383812315fbf
edddddr <endt6342@gmail.com>
2025-06-18 01:40:31 -0900

    with class | inherited

---
+++ Day-22/paddle.py
@@ -0,0 +1,20 @@
+from turtle import Turtle 
+
+class Paddle(Turtle):
+    def __init__(self, postion):
+        super().__init__()
+        self.penup()
+        self.shape('square')
+        self.color('white')
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.setpos(postion)
+
+    def move_up(self):
+        y_position = self.ycor() + 20
+        self.goto(self.xcor(), y_position)
+
+def move_down(self):
+        y_position = self.ycor() - 20
+        self.goto(self.xcor(), y_position)
+
+
