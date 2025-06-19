8bdc868103f944de0c47816127e637b880d806d0
edddddr <endt6342@gmail.com>
2025-06-19 03:42:06 -0900

    The main it controle the all thing goes heree

--- Day-23/main.py
+++ Day-23/main.py
@@ -8,7 +8,35 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+turtle = Player()
+carManager = CarManager()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(turtle.go_up, 'Up')
+
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    carManager.create_car()
+    carManager.move()
+
+    # Detect colisoion with car
+    for car in carManager.all_car:
+        if car.distance(turtle) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # Detect successful crossing
+    if turtle.is_at_finish_line():
+        turtle.got_to_start()
+        carManager.level_up()
+        scoreboard.icrease_level()
+
+
+
+screen.exitonclick()
