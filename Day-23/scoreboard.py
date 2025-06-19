e64a6412d4940c71d3d84d10c2d09eec2b035680
edddddr <endt6342@gmail.com>
2025-06-19 03:44:54 -0900

    Score board display the score and game over text when the turtle hit by the car

--- Day-23/scoreboard.py
+++ Day-23/scoreboard.py
@@ -1,5 +1,27 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 240)
+        self.update_scoreboard()
+        
+    def update_scoreboard(self):
+        self.write(f"Level: {self.level}", align='left', font=FONT)
+
+    def icrease_level(self):
+        self.clear()
+        self.level+=1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"Game Over", align='center', font=FONT)
+        
+
