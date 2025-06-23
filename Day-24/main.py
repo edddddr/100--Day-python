013c61962bd2382ec258016abe71a9404ea52fe1
edddddr <endt6342@gmail.com>
2025-06-22 23:41:10 -0900

    read and write file

--- Day-24/main.py
+++ Day-24/main.py
@@ -1,53 +1,7 @@
-from turtle import  Screen
-from snake import Snake
-from food import Food
-from scoreboard import Score
-import time
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+#     file.close()
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("My Snake Game 🐍🎮")
-screen.tracer(0)
-
-
-snake = Snake()
-food = Food()
-score = Score()
-
-segments = []
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
-
-
-
-game_is_on = True
-
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
-
-
-    # Detect collision with food
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        score.increase_socre()
-        snake.extend()
-
-    # Detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        score.game_over()
-        
-    # Detect collision whiht tail.
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-             game_is_on = False
-             score.game_over()
-        
-        
-
-screen.exitonclick()
+with open("my_file.txt", mode='w') as file:
+    file.write('\n New text')
