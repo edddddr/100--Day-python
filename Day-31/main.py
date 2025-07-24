b7b99750c9d6961aaf9d7aebf82ad0345c4e7348
edddddr <endt6342@gmail.com>
2025-07-24 00:28:14 -0900

    UI section

---
+++ Day-31/main.py
@@ -0,0 +1,37 @@
+from tkinter import *
+import pandas
+from random import choice
+
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
+
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+
+card_img = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 40, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+
+cross_img = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_img, highlightthickness=0,command=next_card)
+unknown_button.grid(row=1, column=0, )
+
+
+check_img = PhotoImage(file="images/right.png")
+known_button = Button(image=check_img, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
+
+next_card()
+
+window.mainloop()
