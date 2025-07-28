be924fe73afc156a1ebad81ae96b1fd0bc86624c
edddddr <endt6342@gmail.com>
2025-07-27 22:03:27 -0900

    quotes of the day

--- Day-32/main.py
+++ Day-32/main.py
@@ -1,12 +1,20 @@
 import smtplib
+import datetime as dt
+import random
+
+now = dt.datetime.now()
+weeday = now.weekday()
 
 my_email = "appbreweryinfo@gmail.com"
 password = "Vejoaewrfu9*0E"
-
-with smtplib.SMTP("smtp.gmailcom") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(form_addr=my_email, 
-                        to_addrs="appbrewerytesting@yahoo.com", 
-                        msg="Subject:Hello\n\nThis is the body of my email."
-                        )
+if weeday == 1:
+    with open("quotes.tx") as quotes_files:
+        all_quotes = quotes_files.readlines()
+        quote_of_the_day  = random.choice(all_quotes)
+    with smtplib("smtp.gmailcom") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(form_addr=my_email, 
+                            to_addrs="appbrewerytesting@yahoo.com", 
+                            msg=f"Subject:Unknown{quote_of_the_day}"
+                            ) 
