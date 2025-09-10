83b53bfd68b97089bc5844271931bbb60d2d656f
edddddr <endt6342@gmail.com>
2025-09-10 01:43:49 -0900

    main files

--- Day-32/main.py
+++ Day-32/main.py
@@ -1,20 +1,30 @@
 import smtplib
-import datetime as dt
 import random
+import datetime as dt
 
-now = dt.datetime.now()
-weeday = now.weekday()
+import pandas
 
-my_email = "appbreweryinfo@gmail.com"
-password = "Vejoaewrfu9*0E"
-if weeday == 1:
-    with open("quotes.tx") as quotes_files:
-        all_quotes = quotes_files.readlines()
-        quote_of_the_day  = random.choice(all_quotes)
-    with smtplib("smtp.gmailcom") as connection:
+MY_EMAIL = "endt6342@gmail.com"
+MY_PASSWORD = "cnbqubgpmobilcjt"
+
+
+today = dt.datetime.now()
+today_tuple = (today.month, today.day)
+data = pandas.read_csv("birthdays.csv")
+
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for(index, data_row) in data.iterrows()}
+if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents.replace("[Name]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
-        connection.login(user=my_email, password=password)
-        connection.sendmail(form_addr=my_email, 
-                            to_addrs="appbrewerytesting@yahoo.com", 
-                            msg=f"Subject:Unknown{quote_of_the_day}"
-                            ) 
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs="ermaisthomas97",
+            msg=f"Subject: Happy Birthday\n\n{contents}"
+        )
